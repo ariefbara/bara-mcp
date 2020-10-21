1a90d8df95f2470939ac4726c57c9ae847d74934
adi purnama <purnama.adi@gmail.com>
2020-10-21 16:22:06 +0700

    [error fix] Consultant can view Worksheet

--- app/Http/Controllers/Personnel/AsProgramConsultant/Participant/WorksheetController.php
+++ app/Http/Controllers/Personnel/AsProgramConsultant/Participant/WorksheetController.php
@@ -9,7 +9,8 @@ use App\Http\Controllers\ {
 use Query\ {
     Application\Service\Firm\Program\Participant\ViewWorksheet,
     Domain\Model\Firm\Program\Mission,
-    Domain\Model\Firm\Program\Participant\Worksheet
+    Domain\Model\Firm\Program\Participant\Worksheet,
+    Infrastructure\QueryFilter\WorksheetFilter
 };
 
 class WorksheetController extends AsProgramConsultantBaseController
@@ -19,38 +20,61 @@ class WorksheetController extends AsProgramConsultantBaseController
         $this->authorizedPersonnelIsProgramConsultant($programId);
         
         $service = $this->buildViewService();
-        $worksheet = $service->showById($this->firmId(), $programId, $participantId, $worksheetId);
+        $worksheet = $service->showById($programId, $worksheetId);
         
         return $this->singleQueryResponse($this->arrayDataOfWorksheet($worksheet));
     }
+    
     public function showAll($programId, $participantId)
     {
         $this->authorizedPersonnelIsProgramConsultant($programId);
+
         $service = $this->buildViewService();
-        
-        $worksheets = $service->showAll($this->firmId(), $programId, $participantId, $this->getPage(), $this->getPageSize());
-        
+        $worksheetFilter = (new WorksheetFilter())
+                ->setMissionId($this->stripTagQueryRequest("missionId"))
+                ->setParentId($this->stripTagQueryRequest("parentId"))
+                ->setHasParent($this->filterBooleanOfQueryRequest("hasParent"));
+
+        $worksheets = $service->showAll(
+                $programId, $participantId, $this->getPage(), $this->getPageSize(), $worksheetFilter);
+
         $result = [];
         $result['total'] = count($worksheets);
         foreach ($worksheets as $worksheet) {
+            $parent = empty($worksheet->getParent()) ? null : [
+                "id" => $worksheet->getParent()->getId(),
+                "name" => $worksheet->getParent()->getName(),
+            ];
             $result['list'][] = [
                 "id" => $worksheet->getId(),
                 "name" => $worksheet->getName(),
-                "mission" => $this->arrayDataOfMission($worksheet->getMission()),
-                "parent" => $this->arrayDataOfParentWorksheet($worksheet->getParent()),
+                "mission" => [
+                    "id" => $worksheet->getMission()->getId(),
+                    "name" => $worksheet->getMission()->getName(),
+                    "description" => $worksheet->getMission()->getDescription(),
+                ],
+                "parent" => $parent,
             ];
         }
         return $this->listQueryResponse($result);
     }
-    
+
     protected function arrayDataOfWorksheet(Worksheet $worksheet): array
     {
         $data = (new FormRecordToArrayDataConverter())->convert($worksheet);
         $data['id'] = $worksheet->getId();
         $data['name'] = $worksheet->getName();
-        $data['mission'] = $this->arrayDataOfMission($worksheet->getMission());
         $data['parent'] = $this->arrayDataOfParentWorksheet($worksheet->getParent());
+        $data['mission'] = $this->arrayDataOfMission($worksheet->getMission());
         return $data;
+    }
+    protected function arrayDataOfParentWorksheet(?Worksheet $parentWorksheet): ?array
+    {
+        return empty($parentWorksheet) ? null : [
+            "id" => $parentWorksheet->getId(),
+            "name" => $parentWorksheet->getName(),
+            "parent" => $this->arrayDataOfParentWorksheet($parentWorksheet->getParent()),
+        ];
     }
     protected function arrayDataOfMission(Mission $mission): array
     {
@@ -58,18 +82,14 @@ class WorksheetController extends AsProgramConsultantBaseController
             "id" => $mission->getId(),
             "name" => $mission->getName(),
             "description" => $mission->getDescription(),
+            "worksheetForm" => [
+                "id" => $mission->getWorksheetForm()->getId(),
+                "name" => $mission->getWorksheetForm()->getName(),
+                "description" => $mission->getWorksheetForm()->getDescription(),
+            ],
         ];
     }
-    protected function arrayDataOfParentWorksheet(?Worksheet $worksheet): ?array
-    {
-        if (empty($worksheet)) {
-            return null;
-        }
-        return [
-            "id" => $worksheet->getId(),
-            "name" => $worksheet->getName(),
-        ];
-    }
+    
     protected function buildViewService()
     {
         $worksheetRepository = $this->em->getRepository(Worksheet::class);
--- tests/Controllers/Personnel/AsProgramConsultant/Participant/WorksheetControllerTest.php
+++ tests/Controllers/Personnel/AsProgramConsultant/Participant/WorksheetControllerTest.php
@@ -39,7 +39,7 @@ class WorksheetControllerTest extends WorksheetTestCase
         parent::tearDown();
     }
     
-    public function test_show()
+    public function test_show_200()
     {
         $response = [
             "id" => $this->worksheetOne->id,
@@ -48,11 +48,16 @@ class WorksheetControllerTest extends WorksheetTestCase
                 "id" => $this->worksheetOne->mission->id,
                 "name" => $this->worksheetOne->mission->name,
                 "description" => $this->worksheetOne->mission->description,
-                
+                "worksheetForm" => [
+                    "id" => $this->worksheetOne->mission->worksheetForm->id,
+                    "name" => $this->worksheetOne->mission->worksheetForm->form->name,
+                    "description" => $this->worksheetOne->mission->worksheetForm->form->description,
+                ],
             ],
             "parent" => [
                 "id" => $this->worksheetOne->parent->id,
                 "name" => $this->worksheetOne->parent->name,
+                "parent" => null,
             ],
             "submitTime" => $this->worksheetOne->formRecord->submitTime,
             "stringFieldRecords" => [],
@@ -67,14 +72,14 @@ class WorksheetControllerTest extends WorksheetTestCase
                 ->seeStatusCode(200)
                 ->seeJsonContains($response);
     }
-    public function test_show_personnelNotProgramConsultant()
+    public function test_show_inactiveConsultant_401()
     {
         $uri = $this->worksheetUri . "/{$this->worksheetOne->id}";
         $this->get($uri, $this->removedConsultant->personnel->token)
                 ->seeStatusCode(401);
     }
     
-    public function test_showAll()
+    public function test_showAll_200()
     {
         $response = [
             "total" => 2,
@@ -109,11 +114,85 @@ class WorksheetControllerTest extends WorksheetTestCase
         $this->get($this->worksheetUri, $this->consultant->personnel->token)
                 ->seeStatusCode(200)
                 ->seeJsonContains($response);
-     }
-     public function test_showAll_personnelNotProgramConsultant_error401()
-     {
+    }
+    public function test_showAll_inactiveConsultant_401()
+    {
         $this->get($this->worksheetUri, $this->removedConsultant->personnel->token)
                 ->seeStatusCode(401);
-         
-     }
+    }
+    public function test_showAll_hasParentFilterSet_200()
+    {
+        $response = [
+            "total" => 1,
+            "list" => [
+                [
+                    "id" => $this->worksheet->id,
+                    "name" => $this->worksheet->name,
+                    "mission" => [
+                        "id" => $this->worksheet->mission->id,
+                        "name" => $this->worksheet->mission->name,
+                        "description" => $this->worksheet->mission->description,
+
+                    ],
+                    "parent" => null,
+                ],
+            ],
+        ];
+        $uri = $this->worksheetUri . "?hasParent=false";
+        $this->get($uri, $this->consultant->personnel->token)
+                ->seeJsonContains($response)
+                ->seeStatusCode(200);
+    }
+    public function test_showAll_missionIdFilterSet_200()
+    {
+        $response = [
+            "total" => 1,
+            "list" => [
+                [
+                    "id" => $this->worksheetOne->id,
+                    "name" => $this->worksheetOne->name,
+                    "mission" => [
+                        "id" => $this->worksheetOne->mission->id,
+                        "name" => $this->worksheetOne->mission->name,
+                        "description" => $this->worksheetOne->mission->description,
+
+                    ],
+                    "parent" => [
+                        "id" => $this->worksheetOne->parent->id,
+                        "name" => $this->worksheetOne->parent->name,
+                    ],
+                ],
+            ],
+        ];
+        $uri = $this->worksheetUri . "?missionId={$this->worksheetOne->mission->id}";
+        $this->get($uri, $this->consultant->personnel->token)
+                ->seeJsonContains($response)
+                ->seeStatusCode(200);
+    }
+    public function test_showAll_parentIdFilterSet_200()
+    {
+        $response = [
+            "total" => 1,
+            "list" => [
+                [
+                    "id" => $this->worksheetOne->id,
+                    "name" => $this->worksheetOne->name,
+                    "mission" => [
+                        "id" => $this->worksheetOne->mission->id,
+                        "name" => $this->worksheetOne->mission->name,
+                        "description" => $this->worksheetOne->mission->description,
+
+                    ],
+                    "parent" => [
+                        "id" => $this->worksheetOne->parent->id,
+                        "name" => $this->worksheetOne->parent->name,
+                    ],
+                ],
+            ],
+        ];
+        $uri = $this->worksheetUri . "?parentId={$this->worksheet->id}";
+        $this->get($uri, $this->consultant->personnel->token)
+                ->seeJsonContains($response)
+                ->seeStatusCode(200);
+    }
 }
