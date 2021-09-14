c57611a2058fbe09e58243ae8d4b57210237f044
ariefbara <51996443+ariefbara@users.noreply.github.com>
2021-09-14 09:36:32 +0700

    Update dev.konsulta.co

--- src/Config/BaseConfig.php
+++ src/Config/BaseConfig.php
@@ -10,7 +10,7 @@ class BaseConfig
     const MAIL_SENDER_ADDRESS = 'noreply@innovid.xyz';
     const MAIL_SENDER_NAME = 'innovation';
     const KONSULTA_LOGO_PATH = "";
-    const KONSULTA_MAIN_URL = "http://konsulta.co";
-    const KONSULTA_STORAGE_URL = "http://konsulta.co/praja-mcp/storage/app";
+    const KONSULTA_MAIN_URL = "http://dev.konsulta.co";
+    const KONSULTA_STORAGE_URL = "http://dev.konsulta.co/bara-mcp/storage/app";
 
 }
