df9362f70f104fad9679810063eb68c2219d9e6f
ariefbara <51996443+ariefbara@users.noreply.github.com>
2021-09-28 11:02:44 +0700

    Update BaseConfig.php

--- src/Config/BaseConfig.php
+++ src/Config/BaseConfig.php
@@ -19,8 +19,8 @@ class BaseConfig
     // const KONSULTA_STORAGE_URL = "http://innovid.xyz/praja-mcp/storage/app";
     // const KONSULTA_MAIN_URL = "http://innov.id";
     // const KONSULTA_STORAGE_URL = "http://innov.id/bara-mcp/storage/app";
-    const KONSULTA_MAIN_URL = "http://hub.innov.id";
-    const KONSULTA_STORAGE_URL = "http://hub.innov.id/bara-mcp/storage/app";
+    const KONSULTA_MAIN_URL = "http://app.innov.id";
+    const KONSULTA_STORAGE_URL = "http://app.innov.id/bara-mcp/storage/app";
     // const KONSULTA_MAIN_URL = "http://konsulta.co";
     // const KONSULTA_STORAGE_URL = "http://konsulta.co/praja-mcp/storage/app";
     // const KONSULTA_MAIN_URL = "http://app.innov.web.id";
