a68d111c96c705b0d526094fd66699eecc1182d6
ariefbara <51996443+ariefbara@users.noreply.github.com>
2021-09-29 22:34:21 +0700

    Update BaseConfig.php

--- src/Config/BaseConfig.php
+++ src/Config/BaseConfig.php
@@ -7,8 +7,8 @@ use SharedContext\Domain\ValueObject\MailMessage;
 class BaseConfig
 {
 
-    const MAIL_SENDER_ADDRESS = 'noreply@innov.id';
-    // const MAIL_SENDER_ADDRESS = 'noreply@konsulta.co';
+    // const MAIL_SENDER_ADDRESS = 'noreply@innov.id';
+    const MAIL_SENDER_ADDRESS = 'noreply@konsulta.co';
     const MAIL_SENDER_NAME = 'Admin';
     const KONSULTA_LOGO_PATH = "";
     // const KONSULTA_MAIN_URL = "http://dev.konsulta.co";
@@ -19,10 +19,10 @@ class BaseConfig
     // const KONSULTA_STORAGE_URL = "http://innovid.xyz/praja-mcp/storage/app";
     // const KONSULTA_MAIN_URL = "http://innov.id";
     // const KONSULTA_STORAGE_URL = "http://innov.id/bara-mcp/storage/app";
-    const KONSULTA_MAIN_URL = "http://app.innov.id";
-    const KONSULTA_STORAGE_URL = "http://app.innov.id/bara-mcp/storage/app";
-    // const KONSULTA_MAIN_URL = "http://konsulta.co";
-    // const KONSULTA_STORAGE_URL = "http://konsulta.co/praja-mcp/storage/app";
+    // const KONSULTA_MAIN_URL = "http://app.innov.id";
+    // const KONSULTA_STORAGE_URL = "http://app.innov.id/bara-mcp/storage/app";
+    const KONSULTA_MAIN_URL = "http://konsulta.co";
+    const KONSULTA_STORAGE_URL = "http://konsulta.co/praja-mcp/storage/app";
     // const KONSULTA_MAIN_URL = "http://app.innov.web.id";
     // const KONSULTA_STORAGE_URL = "http://app.innov.web.id/bara-mcp/storage/app";
 
